258049227dd2d9e206c4c2a539cce5739747dee3
ryanfear <impulse.fx@gmail.com>
2018-12-04 11:03:32 -0700

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,107 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/ryanfear",
+    "name": "Ryan Raef",
+    "role": "",
+    "linkedinId":"",
+    "skypeid": "",
+    "roleDescription": "I Enjoy doing lots of things. Climbing. Reading. Gaming. And coding which is what this site is all about",
+    "socialLinks":[
+        // {
+        //   "name":"linkedin",
+        //   "url":"",
+        //   "className":"fa fa-linkedin"
+        // },
+        {
+          "name":"github",
+          "url":"https://github.com/ryanfear",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"",
+    "address":"Denver, CO",
+    "website":"https://ryanfear.github.io",
+    "education":[
+      {
+        "UniversityName":"University of North Texas",
+        "specialization":"Bachelors of Science, Geography",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2008-2010",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Cedar Valley Community College",
+        "specialization":"Computer Animation",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2005-2007",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"IT Consulting",
+        "specialization":"Self Employed",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2013-Current",
+        "Achievements":"Linux administration; managing various systems for independent projects and web based solutions",
+        //"Achievements":"Consulting on e-commerce solutions for local businesses. Performing analysis of business models and presenting (then implementing) solutions based on client needs"
+      },
+      {
+        "CompanyName":"Specialty Coffee Industry",
+        "specialization":"Various Employers",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"2010-2016",
+        "Achievements":"Designed and implemented training programs with a heavy focus on customer service and drink preparation in fast paced environments"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"javascript" + " (ES6)"
+      },
+      {
+        "skillname":"reactjs"
+      },
+      {
+        "skillname":"bash" + " / powershell / perl"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
